1533c6831fdb011875eb6fbdee9acd5ca01acc15
Mathew Thomas <mattazoid@gmail.com>
2015-11-17 15:39:21 -0500

    updates

--- README.md
+++ README.md
@@ -25,6 +25,7 @@ Next run the application using
     curl http://localhost:8081/metrics
     
 ### Eureka Service Registry
-Make sure to run an instance of Eureka on the local machine. You can then get view Eureka console at...
+Make sure to run an instance of Eureka on the local machine. You can then get view Eureka console at.
+
     http://localhost:8761/eureka
     http://localhost:8761/eureka/apps
--- src/main/java/com/aver/Application.java
+++ src/main/java/com/aver/Application.java
@@ -2,11 +2,13 @@ package com.aver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by mathew on 10/25/15.
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
